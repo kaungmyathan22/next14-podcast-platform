0c487b50e9440a346e62b4c92f5e39153e4bf379
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-10-08 15:18:34 +0630

    feat(): root layout design configured

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,4 +1,7 @@
+import LeftSidebar from "@/components/LeftSidebar";
+import RightSidebar from "@/components/RightSidebar";
 import type { Metadata } from "next";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -11,10 +14,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      <main>
-        <p className="text-white-1">LEFT SIDEBar</p> {children}
-        <p className="text-white-1">right sidebar</p>
+    <div className="relative flex flex-col">
+      <main className="relative flex bg-black-3">
+        <LeftSidebar />
+        <section className="flex min-h-screen flex-1 flex-col px-4 sm:px-14">
+          <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+            <div className="flex h-16 items-center justify-between md:hidden">
+              <Image
+                src="/icons/logo.svg"
+                width={30}
+                height={30}
+                alt="menu icon"
+              />
+              {/* <MobileNav /> */}
+            </div>
+            <div className="flex flex-col md:pb-14">
+              {/* <Toaster /> */}
+
+              {children}
+            </div>
+          </div>
+        </section>
+        <RightSidebar />
       </main>
     </div>
   );
