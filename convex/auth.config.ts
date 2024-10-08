01aad9784a8d1eeeedbb37bfaf179caecdab91d3
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-10-08 15:53:32 +0630

    feat(): convex cleark integrated

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://helped-gelding-0.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
