5f058cd73355dc5a13800329925ddd71c3ff45d9
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-10-08 15:18:50 +0630

    update(): home page added

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,26 @@
+import PodcastCard from "@/components/PodcastCard";
+import { podcastData } from "@/constants";
+
 export default function Home() {
   return (
-    <div className="mt-9 flex flex-col gap-9">
-      <div className="flex flex-col gap-5">
+    <div className="mt-9 flex flex-col gap-9 md:overflow-hidden">
+      <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
-      </div>
+
+        <div className="podcast_grid">
+          {podcastData?.map(
+            ({ _id, podcastTitle, podcastDescription, imageUrl }) => (
+              <PodcastCard
+                key={_id}
+                imgUrl={imageUrl as string}
+                title={podcastTitle}
+                description={podcastDescription}
+                podcastId={_id}
+              />
+            )
+          )}
+        </div>
+      </section>
     </div>
   );
 }
