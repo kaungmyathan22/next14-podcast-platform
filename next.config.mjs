8d9e9c50963bd16da678a87c0c1f27e35f94c868
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-10-12 15:46:12 +0630

    update(): whitelist domain

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,15 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
         hostname: "lovely-flamingo-139.convex.cloud",
+      },
+      {
+        protocol: "https",
+        hostname: "wary-manatee-658.convex.cloud",
       },
     ],
   },
